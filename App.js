85d2ec43b32ce7cc1ef7b31d88316eb2c221124c
teobot <theoclapperton@outlook.com>
2021-02-02 01:28:54 +0000

    Add Location Screen, Logout button, Changed Logic

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
+import React from "react";
+
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import CreateAccountScreen from "./src/screens/CreateAccountScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SearchScreen from "./src/screens/SearchScreen";
+import LocationScreen from "./src/screens/LocationScreen";
+
+import LogoutButton from "./src/components/LogoutButton";
 
 const navigator = createStackNavigator(
   {
-    Login: LoginScreen,
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: {
+        title: "Login",
+      },
+    },
     CreateAccount: CreateAccountScreen,
     Search: {
       screen: SearchScreen,
       navigationOptions: {
         headerLeft: () => null,
+        headerRight: () => <LogoutButton />,
+      },
+    },
+    Location: {
+      screen: LocationScreen,
+      navigationOptions: {
+        headerRight: () => <LogoutButton />,
+        title: "Something new",
       },
     },
   },
@@ -24,4 +42,7 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const AppContainer = createAppContainer(navigator);
+
+// Now AppContainer is the main component for React to render
+export default AppContainer;
