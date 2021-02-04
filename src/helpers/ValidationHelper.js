d0a638172ca85f93ffd6eedfaf600945aea35a3d
teobot <theoclapperton@outlook.com>
2021-02-04 02:40:22 +0000

    Added comments

--- src/helpers/ValidationHelper.js
+++ src/helpers/ValidationHelper.js
@@ -1,8 +1,15 @@
 import Filter from "bad-words";
 
 export default class ValidationHelper {
+  /**
+   *This method returns TRUE if the given JSON object includes any bad words ELSE false
+   *
+   * @static
+   * @param {*} array
+   * @returns TRUE if the given JSON object includes no bad words, else FALSE
+   * @memberof ValidationHelper
+   */
   static ValidateCreateAccountForm = (array) => {
-    // console.log("Validating given javascript object!");
     const filter = new Filter();
     return filter.isProfane(JSON.stringify(array));
   };
