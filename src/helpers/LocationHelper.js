543a9acba60ec4be72d85bf7cdc04df9e876610e
teobot <theoclapperton@outlook.com>
2021-02-08 01:59:22 +0000

    Added new helper

---
+++ src/helpers/LocationHelper.js
@@ -0,0 +1,13 @@
+import * as Location from "expo-location";
+
+export default class LocationHelper {
+  static getLocation = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    return location
+  };
+}
