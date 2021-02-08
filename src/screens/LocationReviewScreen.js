786b47c3e0da0fe82f772870d80edf3b1226bf41
teobot <theoclapperton@outlook.com>
2021-02-08 01:59:56 +0000

    Added new component

---
+++ src/screens/LocationReviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const LocationReviewScreen = () => {
+    return (
+    <View>
+      <Text>Welcome, To the LocationReviewScreen react native component!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default LocationReviewScreen;
