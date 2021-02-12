a313dda058cf79d9c60a80959d59e82f5be209b8
teobot <theoclapperton@outlook.com>
2021-02-12 15:28:48 +0000

    Added new component for deleting a review

---
+++ src/components/DeleteReview.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { Button, Overlay } from "react-native-elements";
+
+import coffida from "../api/coffida";
+
+const DeleteReview = ({ navigation }) => {
+  const location_id = navigation.state.params.locationId;
+  const review_id = navigation.state.params.userReview.review_id;
+  const [overlay, setOverlay] = useState(false);
+
+  const handleReviewDelete = async () => {
+    // /location/{loc_id}/review/{rev_id}
+    try {
+      const response = await coffida.delete(
+        `/location/${location_id}/review/${review_id}`
+      );
+      if (response.status === 200) {
+        // Deletion was successful
+        navigation.navigate("Location", {
+          location_id,
+        });
+      } else {
+        // TODO: Deletion of the review has failed
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+      // TODO: Deletion of the review networking has failed
+    }
+  };
+  return (
+    <TouchableOpacity style={{ padding: 10 }} onPress={() => setOverlay(true)}>
+      <Overlay isVisible={overlay}>
+        <View style={{ padding: 5, margin: 5 }}>
+          <View>
+            <Text>Are you sure you want to delete your review?</Text>
+          </View>
+          <View style={{ padding: 5, margin: 5, flexDirection: "row" }}>
+            <View style={{ flex: 1, alignItems: "center" }}>
+              <Button
+                raised
+                buttonStyle={styles.buttonStyle}
+                type="outline"
+                title="yes"
+                onPress={handleReviewDelete}
+              />
+            </View>
+            <View style={{ flex: 1, alignItems: "center" }}>
+              <Button
+                buttonStyle={styles.buttonStyle}
+                onPress={() => setOverlay(false)}
+                type="outline"
+                title="no"
+                raised
+              />
+            </View>
+          </View>
+        </View>
+      </Overlay>
+      <MaterialCommunityIcons name="delete-forever" size={36} color="black" />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputStyle: {
+    fontSize: 18,
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 5,
+    margin: 5,
+  },
+  buttonStyle: {
+    padding: 5,
+    width: 100,
+    color: "red",
+  },
+});
+
+export default DeleteReview;
