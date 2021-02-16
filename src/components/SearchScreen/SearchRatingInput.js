50b60a4cb3b3ddc96756eab09bbea318d6fb0b6d
teobot <theoclapperton@outlook.com>
2021-02-16 00:45:24 +0000

    Added new rating component

---
+++ src/components/SearchScreen/SearchRatingInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Rating } from "react-native-elements";
+
+const SearchRatingInput = ({
+  title,
+  valueTitle,
+  value,
+  dispatcher,
+  backgroundColor,
+}) => {
+  return (
+    <View style={{ alignItems: "center" }}>
+      <View style={{ padding: 5 }}>
+        <Text>{title}</Text>
+        <Rating
+          type="custom"
+          tintColor={backgroundColor}
+          imageSize={30}
+          onFinishRating={(newValue) =>
+            dispatcher({ type: `change_${valueTitle}`, payload: newValue })
+          }
+          showRating={false}
+          startingValue={value}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchRatingInput;
