b5fd4d28208614f4aa9851ed1f96f30ddd802641
teobot <theoclapperton@outlook.com>
2021-02-16 18:41:06 +0000

    Changed location image to database image, Removed comments

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -15,7 +15,7 @@ const LocationCard = ({ item, navigation }) => {
     avg_overall_rating,
     location_reviews,
     location_id,
-    latitude,
+    photo_path,
   } = item;
 
   return (
@@ -30,7 +30,7 @@ const LocationCard = ({ item, navigation }) => {
         <Image
           style={styles.imageStyle}
           source={{
-            uri: `https://source.unsplash.com/250x120/?coffee,shop?sig=${location_id}${latitude}`,
+            uri: `${photo_path}?sig=${location_name}`,
           }}
         />
         <Text style={styles.name}>{location_name}</Text>
@@ -65,57 +65,4 @@ const styles = StyleSheet.create({
   },
 });
 
-// const styles = StyleSheet.create({
-//     imageStyle: {
-//       width: 250,
-//       height: 120,
-//       borderRadius: 5,
-//       marginBottom: 5,
-//     },
-//     name: {
-//       fontWeight: "bold",
-//       fontSize: 16,
-//     },
-//     container: {
-//       marginHorizontal: 15,
-//     },
-//   });
-
 export default withNavigation(LocationCard);
-
-//   {
-//     avg_clenliness_rating: 2.5,
-//     avg_overall_rating: 3,
-//     avg_price_rating: 3,
-//     avg_quality_rating: 3,
-//     latitude: 80,
-//     location_id: 4,
-//     location_name: "Ben's Diner",
-//     location_reviews: [
-//       {
-//         likes: 3,
-//         review_body: "Grim, and expensive",
-//         review_clenlinessrating: 0,
-//         review_id: 2,
-//         review_location_id: 4,
-//         review_overallrating: 1,
-//         review_pricerating: 1,
-//         review_qualityrating: 1,
-//         review_user_id: 1,
-//       },
-//       {
-//         likes: 0,
-//         review_body: "Not sure what the problem is, I love it here",
-//         review_clenlinessrating: 5,
-//         review_id: 5,
-//         review_location_id: 4,
-//         review_overallrating: 5,
-//         review_pricerating: 5,
-//         review_qualityrating: 5,
-//         review_user_id: 6,
-//       },
-//     ],
-//     location_town: "London",
-//     longitude: 0,
-//     photo_path: "http://cdn.dummyphoto.com",
-//   },
