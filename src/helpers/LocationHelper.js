461a279f263827ff3b65ef50af6b8703022cc655
teobot <theoclapperton@outlook.com>
2021-02-16 00:46:30 +0000

    Added new getDistanceToCoords function

--- src/helpers/LocationHelper.js
+++ src/helpers/LocationHelper.js
@@ -1,14 +1,30 @@
 import * as Location from "expo-location";
+import { getDistance } from "geolib";
 
 export default class LocationHelper {
   static getLocation = async () => {
     let { status } = await Location.requestPermissionsAsync();
     if (status !== "granted") {
       setErrorMsg("Permission to access location was denied");
-      return;
+      return null;
     }
     let location = await Location.getCurrentPositionAsync({});
-    return location
+    return location;
   };
 
+  static getDistanceToCoords = async ({ lat, long }) => {
+    const userLocation = await this.getLocation();
+    if (userLocation !== null) {
+      const distance = await getDistance(
+        { latitude: lat, longitude: long },
+        {
+          latitude: userLocation.coords.latitude,
+          longitude: userLocation.coords.longitude,
+        }
+      );
+      return distance;
+    } else {
+      return null;
+    }
+  };
 }
