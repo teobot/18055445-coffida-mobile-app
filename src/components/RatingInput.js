bee012cee1eaaacc062f18d0ddb41d12e856ac9a
teobot <theoclapperton@outlook.com>
2021-02-16 00:43:44 +0000

    Added extra customization

--- src/components/RatingInput.js
+++ src/components/RatingInput.js
@@ -2,17 +2,30 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Rating } from "react-native-elements";
 
-const RatingInput = ({ title, valueTitle, value, dispatcher }) => {
+const RatingInput = ({
+  title,
+  valueTitle,
+  value,
+  dispatcher,
+  containerMargin,
+  containerPadding,
+  showRating,
+}) => {
   return (
-    <View style={{ padding: 15, marginVertical: 5 }} key={title}>
+    <View
+      style={{
+        padding: containerPadding !== undefined ? containerPadding : 15,
+        marginVertical: containerMargin !== undefined ? containerMargin : 5,
+      }}
+      key={title}
+    >
       <Text>{title}</Text>
       <Rating
         startingValue={value}
-        onFinishRating={(val) =>
-          dispatcher({ type: `change_${valueTitle}`, payload: val })
+        onFinishRating={(newValue) =>
+          dispatcher({ type: `change_${valueTitle}`, payload: newValue })
         }
-        showRating
-        startingValue={value}
+        showRating={showRating !== undefined ? showRating : true}
       />
     </View>
   );
