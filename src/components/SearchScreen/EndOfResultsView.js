91620d930884e503717da23743fc266c2fc20718
teobot <theoclapperton@outlook.com>
2021-02-20 23:53:05 +0000

    Created new End of results text, implemented dark mode

--- src/components/SearchScreen/EndOfResultsView.js
+++ src/components/SearchScreen/EndOfResultsView.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
 
 import { Divider, Text } from "react-native-elements";
+import { ThemeContext } from "../../context/ThemeContext";
 
-const EndOfResultsView = () => {
+const EndOfResultsView = ({ results }) => {
+  const { Theme } = useContext(ThemeContext);
+  const styles = StyleSheet.create({
+    ThemeTextColor: {
+      color: Theme === "dark" ? "white" : "black",
+    },
+    ResultsText: {
+      alignSelf: "center",
+      color: Theme === "dark" ? "white" : "black",
+    },
+  });
   return (
-    <View style={{padding: 2, margin: 2,}}>
+    <View style={{ padding: 2, margin: 2 }}>
       <Divider />
-      <Text style={{ fontSize: 14, alignSelf: "center" }}>bottom of results :)</Text>
+      {results.length > 0 ? (
+        <Text h4 style={styles.ResultsText}>
+          bottom of results :)
+        </Text>
+      ) : (
+        <Text h4 style={styles.ResultsText}>
+          No results matched search!
+        </Text>
+      )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default EndOfResultsView;
