7388a0065181c94f84659592318abddccdc5472e
teobot <theoclapperton@outlook.com>
2021-02-21 00:03:24 +0000

    Changed component imports, Changed reducer type names, Refactored account information validation, Added Toast messages, Redesign screen styling

--- src/screens/CreateAccountScreen.js
+++ src/screens/CreateAccountScreen.js
@@ -1,28 +1,31 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer, useState, useContext, createRef } from "react";
 import {
   View,
-  Text,
   StyleSheet,
-  Button,
-  TextInput,
+  KeyboardAvoidingView,
   TouchableOpacity,
 } from "react-native";
-import { withNavigation } from "react-navigation";
+import { Button, Input, Text, Icon } from "react-native-elements";
 import { Feather } from "@expo/vector-icons";
 
+import { withNavigation } from "react-navigation";
+
 import ValidationHelper from "../helpers/ValidationHelper";
+
+import { ThemeContext } from "../context/ThemeContext";
+import { ToastContext } from "../context/ToastContext";
 
 import coffida from "../api/coffida";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "change_LastName":
+    case "change_last_name":
       return { ...state, last_name: action.payload };
-    case "change_FirstName":
+    case "change_first_name":
       return { ...state, first_name: action.payload };
-    case "change_Email":
+    case "change_email":
       return { ...state, email: action.payload };
-    case "change_Password":
+    case "change_password":
       return { ...state, password: action.payload };
     default:
       return state;
@@ -31,115 +34,197 @@ const reducer = (state, action) => {
 
 const CreateAccountScreen = ({ navigation }) => {
   const [state, dispatch] = useReducer(reducer, {
-    first_name: "theo",
-    last_name: "clapperton",
-    email: "theoclapperton@outlook.com",
-    password: "hello1234",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
   });
-  const { first_name, last_name, email, password } = state;
   const [passwordVisible, setPasswordVisible] = useState(true);
+  const { Theme } = useContext(ThemeContext);
+  const { showToast, show404Toast, show500Toast } = useContext(ToastContext);
 
-  const createAccount = async (formData) => {
-    if (ValidationHelper.IsObjectProfane(formData)) {
-      // Something includes bad words
-      // TODO: Bad words detected in the form data
-      console.log("Bad words in the form data :(");
-    } else {
-      // Form data is clean so carry on with the submit.
-      console.log("Submitting the data to Mudfoot");
-      try {
-        const form_submit = await coffida.post("/user", formData);
-        const { email, password } = state;
-        navigation.navigate("Login", {
-          email,
-          password,
+  const handleAccountCreation = async () => {
+    const first_name_errors = ValidationHelper.validator({
+      type: "validate_first_name",
+      payload: state.first_name,
+    });
+    const last_name_errors = ValidationHelper.validator({
+      type: "validate_last_name",
+      payload: state.last_name,
+    });
+    const email_errors = ValidationHelper.validator({
+      type: "validate_email",
+      payload: state.email,
+    });
+    const password_errors = ValidationHelper.validator({
+      type: "validate_password",
+      payload: state.password,
+    });
+
+    if (
+      first_name_errors !== undefined &&
+      last_name_errors !== undefined &&
+      email_errors !== undefined &&
+      password_errors !== undefined
+    ) {
+      // Show of the user input has returned errors
+      let error_message = "";
+      if (first_name_errors !== undefined) {
+        error_message += first_name_errors.first_name[0] + "\n";
+      }
+      if (last_name_errors !== undefined) {
+        error_message += last_name_errors.last_name[0] + "\n";
+      }
+      if (email_errors !== undefined) {
+        error_message += email_errors.email[0] + "\n";
+      }
+      if (password_errors !== undefined) {
+        error_message += password_errors.password[0];
+      }
+      showToast({
+        type: "error",
+        position: "top",
+        text1: "Error with credentials",
+        text2: error_message,
+        visibilityTime: 4000,
+        autoHide: true,
+      });
+      return;
+    }
+
+    // all form information has returned with no errors
+    try {
+      const form_response = await coffida.post("/user", state);
+      console.log(form_response.status);
+
+      if (form_response.status === 201) {
+        showToast({
+          type: "success",
+          position: "top",
+          text1: "Account creation success",
+          text2: "You can now login 👋",
+          visibilityTime: 4000,
+          autoHide: true,
         });
-      } catch (error) {
-        // TODO: something wrong with form submission
-        console.log(error);
+      }
+      const { email, password } = state;
+      navigation.navigate("Login", {
+        email,
+        password,
+      });
+    } catch (error) {
+      // Login request failed
+      if (error.response.status === 400) {
+        // : Bad login credentials
+        show404Toast();
+      } else {
+        // : most likely networking issue
+        show500Toast();
       }
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style={{ margin: 5, padding: 5 }}>
-        Welcome, To the Create Account Screen
-      </Text>
-
-      <TextInput
-        value={first_name}
-        onChangeText={(newTerm) =>
-          dispatch({ type: "change_FirstName", payload: newTerm })
-        }
-        placeholder="First Name"
-        style={styles.inputStyle}
-      />
-
-      <TextInput
-        value={last_name}
-        onChangeText={(newTerm) =>
-          dispatch({ type: "change_LastName", payload: newTerm })
-        }
-        placeholder="Last Name"
-        style={styles.inputStyle}
-      />
-
-      <TextInput
-        value={email}
-        onChangeText={(newTerm) =>
-          dispatch({ type: "change_Email", payload: newTerm })
-        }
-        placeholder="Email"
-        style={styles.inputStyle}
-      />
-
-      <View style={{ flexDirection: "row" }}>
-        <TextInput
-          value={password}
-          secureTextEntry={passwordVisible}
-          onChangeText={(newTerm) =>
-            dispatch({ type: "change_Password", payload: newTerm })
-          }
-          placeholder="Password"
-          style={{ ...styles.inputStyle, flex: 1 }}
-        />
-        <TouchableOpacity
-          style={{ alignSelf: "center", margin: 7 }}
-          onPress={() => setPasswordVisible(!passwordVisible)}
-        >
-          <Feather
-            style={{
-              color: "black",
-              fontSize: 36,
-            }}
-            name={passwordVisible ? "eye-off" : "eye"}
+    <KeyboardAvoidingView behavior="height" style={{ flex: 1, padding: 40 }}>
+      <View style={{ flex: 1, paddingHorizontal: 10 }}>
+        <Text h2 style={{ color: Theme === "dark" ? "whitesmoke" : "black" }}>
+          Create{"\n"}Account
+        </Text>
+      </View>
+      <View
+        style={{ flex: 3, justifyContent: "center", flexDirection: "column" }}
+      >
+        <View style={{ flex: 1 }}>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={state.first_name}
+            onChangeText={(newVal) =>
+              dispatch({ type: "change_first_name", payload: newVal })
+            }
+            placeholder="First name"
           />
-        </TouchableOpacity>
-      </View>
+        </View>
 
-      <View style={styles.viewStyle}>
-        <Button
-          style={styles.buttonStyle}
-          onPress={() => createAccount(state)}
-          title="Create Account"
-        />
+        <View style={{ flex: 1 }}>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={state.last_name}
+            onChangeText={(newVal) =>
+              dispatch({ type: "change_last_name", payload: newVal })
+            }
+            placeholder="Last name"
+          />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={state.email}
+            onChangeText={(newVal) =>
+              dispatch({ type: "change_email", payload: newVal })
+            }
+            placeholder="Email"
+          />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={state.password}
+            secureTextEntry={passwordVisible}
+            onChangeText={(newVal) =>
+              dispatch({ type: "change_password", payload: newVal })
+            }
+            placeholder="Password"
+          />
+          <Icon
+            onPress={() => setPasswordVisible(!passwordVisible)}
+            name={passwordVisible ? "eye-off" : "eye-outline"}
+            type="ionicon"
+            size={36}
+            color="#517fa4"
+          />
+        </View>
       </View>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
+          padding: 10,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Text
+            h4
+            style={{
+              alignSelf: "flex-start",
+              color: Theme === "dark" ? "whitesmoke" : "#222222",
+            }}
+          >
+            Sign up
+          </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Icon
+            raised
+            name="arrow-forward-outline"
+            type="ionicon"
+            reverse={Theme !== "dark"}
+            size={35}
+            containerStyle={{ alignSelf: "flex-end" }}
+            onPress={handleAccountCreation}
+          />
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({
-  inputStyle: {
-    fontSize: 18,
-    borderColor: "black",
-    borderWidth: 2,
-    padding: 5,
-    margin: 5,
-  },
-  viewStyle: {
-    margin: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default withNavigation(CreateAccountScreen);
