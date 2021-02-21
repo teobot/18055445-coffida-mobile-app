86e66c54126049dfcf0a138bba1681c6fc291ff8
teobot <theoclapperton@outlook.com>
2021-02-21 00:00:26 +0000

    Removed legacy form validation system, Replaced with new validation system

--- src/helpers/ValidationHelper.js
+++ src/helpers/ValidationHelper.js
@@ -1,12 +1,9 @@
 import Filter from "bad-words";
-
-const filter = new Filter()
+import { validate } from "validate.js";
 
 export default class ValidationHelper {
-
   /**
-   *This method returns TRUE if the given JSON object includes any bad words ELSE false
-   *
+   * This method returns TRUE if the given JSON object includes any bad words ELSE false
    * @static
    * @param {*} array
    * @returns TRUE if the given JSON object includes no bad words, else FALSE
@@ -17,15 +14,64 @@ export default class ValidationHelper {
   };
 
   static RemoveBadWords = (payload) => {
+    const filter = new Filter();
     return filter.clean(payload);
   };
 
-  static validateInput = ({type, payload}) => {
+  static validateInput = ({ type, payload }) => {
     switch (type) {
       case "validate_password":
-        this.RemoveBadWords(payload).length > 5
+        this.RemoveBadWords(payload).length > 5;
       default:
         return false;
     }
-  }
+  };
+
+  static validator = ({ type, payload }) => {
+    const constraints = {
+      email: {
+        email: {
+          message: "^Please enter a valid email address",
+        },
+      },
+      password: {
+        length: {
+          minimum: 5,
+          message: "^Your password must be at least 5 characters",
+        },
+      },
+      first_name: {
+        length: {
+          minimum: 2,
+          message: "^Your first name must be at least 2 characters",
+        },
+      },
+      last_name: {
+        length: {
+          minimum: 2,
+          message: "^Your last name must be at least 2 characters",
+        },
+      },
+      review_body: {
+        length: {
+          minimum: 10,
+          message: "^Your review must be at least 10 characters",
+        },
+      },
+    };
+    switch (type) {
+      case "validate_email":
+        return validate({ email: payload }, constraints);
+      case "validate_password":
+        return validate({ password: payload }, constraints);
+      case "validate_first_name":
+        return validate({ first_name: payload }, constraints);
+      case "validate_last_name":
+        return validate({ last_name: payload }, constraints);
+      case "validate_review_body":
+        return validate({ review_body: payload }, constraints);
+      default:
+        return undefined;
+    }
+  };
 }
