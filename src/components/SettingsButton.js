25f67393092acb7b6484118942abf08a128b0675
teobot <theoclapperton@outlook.com>
2021-02-22 00:09:06 +0000

    Import ThemeContext import, Added dynamic color changing

--- src/components/SettingsButton.js
+++ src/components/SettingsButton.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { withNavigation } from "react-navigation";
+import { ThemeContext } from "../context/ThemeContext";
 
 const SettingsScreen = ({ navigation }) => {
   const goToSettings = async () => {
     navigation.navigate("Settings");
   };
+  const { Theme } = useContext(ThemeContext);
 
   return (
     <TouchableOpacity onPress={goToSettings}>
       <MaterialIcons
-        style={{ fontSize: 28, padding: 5, margin: 5, color: "black" }}
+        style={{
+          fontSize: 28,
+          padding: 5,
+          margin: 5,
+          color: Theme === "light" ? "#222222" : "whitesmoke",
+        }}
         name="settings"
       />
     </TouchableOpacity>
