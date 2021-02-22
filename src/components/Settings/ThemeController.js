d5a4ecf4c37f5597780d2d03987b5a1633fedac0
teobot <theoclapperton@outlook.com>
2021-02-22 00:14:42 +0000

    Import ThemeContext import, Added dynamic color changing

--- src/components/Settings/ThemeController.js
+++ src/components/Settings/ThemeController.js
@@ -8,10 +8,23 @@ const ThemeController = () => {
   const { Theme, changeTheme } = useContext(ThemeContext);
 
   return (
-    <ListItem bottomDivider>
-      <Icon type="ionicon" name="moon-outline" />
+    <ListItem
+      bottomDivider
+      containerStyle={{
+        backgroundColor: Theme === "dark" ? "#222222" : "white",
+      }}
+    >
+      <Icon
+        color={Theme === "dark" ? "white" : "#222222"}
+        type="ionicon"
+        name={`${Theme === "dark" ? "sunny" : "moon"}-outline`}
+      />
       <ListItem.Content>
-        <ListItem.Title>Dark mode</ListItem.Title>
+        <ListItem.Title
+          style={{ color: Theme === "dark" ? "white" : "#222222" }}
+        >
+          {Theme === "dark" ? "Light" : "Dark"} mode
+        </ListItem.Title>
       </ListItem.Content>
       <Switch value={Theme === "dark"} onValueChange={changeTheme} />
     </ListItem>
