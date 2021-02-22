6e38101dc5d178c1c5d7df7fe9b9eaa05d8672ab
teobot <theoclapperton@outlook.com>
2021-02-22 00:07:20 +0000

    Added ThemeContext import, Added dynamic text color

--- src/components/AccountButton.js
+++ src/components/AccountButton.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { withNavigation } from "react-navigation";
+import { ThemeContext } from "../context/ThemeContext";
 
 const AccountButton = ({ navigation }) => {
   const goToAccount = () => {
     navigation.navigate("Account");
   };
+  const { Theme } = useContext(ThemeContext);
 
   return (
     <TouchableOpacity onPress={goToAccount}>
       <MaterialCommunityIcons
-        style={{ fontSize: 28, padding: 5, margin: 5, color: "black" }}
+        style={{ fontSize: 28, padding: 5, margin: 5, color: Theme === "light" ? "#222222" : "whitesmoke" }}
         name="account"
       />
     </TouchableOpacity>
